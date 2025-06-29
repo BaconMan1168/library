63000cf6cbb6d136d10862309f5f985c60a3f82d
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-06-29 18:45:54 +0800

    Added a function to display books by creating an article element with all the information of a book

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function addBookToLibrary(title, author, genre){
     library.push(new Book(title, author, genre));
 }
 
+function displayBooks(){
+    const bookDisplay = document.querySelector(".book-placeholder");
+    for (let book of library){
+        let bookCard = document.createElement("article");
+        let title = document.createElement("h3");
+        title.textContent = book.title;
+
+        let author = document.createElement("h4");
+        author.textContent = book.author;
+
+        let genre = document.createElement("h4");
+        genre.textContent = book.genre;
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(genre);
+
+        bookDisplay.appendChild(bookCard);
+    }
+}
+
+const newBookBtn = document.querySelector(".new-book");
